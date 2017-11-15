fe3c66f9d4741afbab53145c4bebd674007fb84c
cclarkcode <cclarkrun@gmail.com>
2017-11-14 19:58:38 -0800

    efficiency

--- twoserver.js
+++ twoserver.js
@@ -4,25 +4,25 @@ var goodport = 7000;
 
 var badport = 7500;
 
-function goodrequest (request,response) {
-
-	response.end('You seem like a decent person.');
-}
-
-function badrequest (request,response) {
-
-	response.end('I don\'t know about you. You seem suspect.');
-}
-
-
-var server1 = http.createServer(goodrequest);
-var server2 = http.createServer(badrequest);
+var server1 = http.createServer(handleRequest);
+var server2 = http.createServer(handleRequest);
 
 server1.listen(goodport, listening(goodport));
 server2.listen(badport, listening(badport));
 
-
 function listening(port) {
 
 	console.log('Server running, listening on localhost: ' + port);
+}
+
+function handleRequest (request,response) {
+	var server = request.headers.host.substr(request.headers.host.indexOf(':')+1);
+	
+	if (server === '7000') {
+		response.end('You seem like a decent person.');
+	}
+	else if (server === '7500') {
+		response.end('I don\'t know about you. You seem suspect.');
+	}
+
 }
